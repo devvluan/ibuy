bcc0324d26da1e2859a1ddae75a6857f2f1af4e9
LuanS <luansellera98@gmail.com>
2025-04-06 23:07:15 -0300

    ROUTE - TABS

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,35 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarLabelPosition: "beside-icon",
+        tabBarActiveTintColor: "#000",
+        tabBarInactiveTintColor: "#CECECE",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Sign in",
+          tabBarIcon: ({ color }: { color: any }) => (
+            <MaterialIcons name="login" size={24} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="sign-up"
+        options={{
+          tabBarLabel: "Sign Up",
+          tabBarIcon: ({ color }: { color: any }) => (
+            <MaterialIcons name="person-add" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
